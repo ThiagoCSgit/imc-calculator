01b597061e641ff7ff63f928d87c2007946311d5
ThiagoCSgit <57235729+ThiagoCSgit@users.noreply.github.com>
2023-03-02 16:23:46 -0300

    aula 4 parte 2

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import styles from './styles'
+import React from "react";
+import { View, Text, TouchableOpacity, Share } from "react-native";
+import styles from "./styles";
 
-export default function ResultImc(props){
-    return(
-        <View style={styles.resultImc}>
-            <Text style={styles.message}>{props.messageResultImc}</Text>
-            <Text style={styles.imcValue}>{props.resultImc}</Text>
-        </View>
-    )
+export default function ResultImc(props) {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: `Mensagem enviada pelo app, e meu imc é ${props.resultImc}`,
+    });
+  };
+  return (
+    <View style={styles.resultImc}>
+      <View style={styles.boxShareButton}>
+        {props.resultImc != null && (
+          <TouchableOpacity style={styles.share} onPress={onShare}>
+            <Text style={styles.sharedText}>Compartilhar</Text>
+          </TouchableOpacity>
+        )}
+      </View>
+      <Text style={styles.message}>{props.messageResultImc}</Text>
+      <Text style={styles.imcValue}>{props.resultImc}</Text>
+    </View>
+  );
 }
