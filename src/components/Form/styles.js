a3a252deba5bfbcd0dd3c1f9ecb0c8f9867f1cbb
ThiagoCSgit <57235729+ThiagoCSgit@users.noreply.github.com>
2023-03-02 17:05:52 -0300

    Update styles.js

--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -2,14 +2,12 @@ import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
   formContext: {
-    width: "100%",
-    height: "100%",
-    bottom: 0,
+    flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    marginTop: 30,
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
+    alignItems: "center",
+    paddingTop: 30,
   },
   form: {
     width: "100%",
