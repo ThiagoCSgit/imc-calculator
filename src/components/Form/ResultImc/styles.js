8e58a034e41b38ca7610024530502d2129d1c591
ThiagoCSgit <57235729+ThiagoCSgit@users.noreply.github.com>
2023-03-02 16:24:11 -0300

    aula 4 parte 2

--- src/components/Form/ResultImc/styles.js
+++ src/components/Form/ResultImc/styles.js
@@ -1,24 +1,39 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    resultImc: {
-        flex: 1,
-        marginTop: 15,
-        paddingTop: 30,
-        borderRadius: 50,
-        alignItems: "center",
-        width: "100%",
-    },
-    imcValue: {
-        fontSize: 48,
-        color: "#ff0043",
-        fontWeight: 700
-    },
-    message: {
-        fontSize: 18,
-        color: "#ff0043",
-        fontWeight: 700
-    }
-})
+  resultImc: {
+    flex: 1,
+    marginTop: 15,
+    paddingTop: 30,
+    borderRadius: 50,
+    alignItems: "center",
+    width: "100%",
+  },
+  imcValue: {
+    fontSize: 48,
+    color: "#ff0043",
+    fontWeight: 700,
+  },
+  message: {
+    fontSize: 18,
+    color: "#ff0043",
+    fontWeight: 700,
+  },
+  boxShareButton: {
+    width: "100%",
+    alignItems: "center",
+    marginBottom: 15,
+  },
+  share: {
+    backgroundColor: "#1877f2",
+    borderRadius: 50,
+    paddingVertical: 10,
+  },
+  sharedText: {
+    color: "#fff",
+    fontWeight: 700,
+    paddingHorizontal: 30,
+  },
+});
 
-export default styles
+export default styles;
